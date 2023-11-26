b599bed0046974dc2afa7cf98d7fa93c33b9c66e
Rodrigo Tavares <tavares@netplataforma.com.br>
2023-11-26 17:28:21 -0300

    Projeto com front-end do CRUD de Produtos

---
+++ MarketPlaceAspNet/Pages/CrudProdutos.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MarketPlaceAspNet.Pages
+{
+    public class CrudProdutosModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public CrudProdutosModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
---
+++ MarketPlaceAspNet/Pages/FormProdutos.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MarketPlaceAspNet.Pages
+{
+    public class FormProdutosModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public FormProdutosModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
